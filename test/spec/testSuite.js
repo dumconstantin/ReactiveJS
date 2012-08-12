cf56158e37c2f1f14fa81debc7d4914d30715373
Constantin Dumitrescu <dum.constantin@gmail.com>
2012-08-12 18:41:49 +0300

    Added testing suite.

---
+++ test/spec/testSuite.js
@@ -0,0 +1,48 @@
+describe('Object registration and validation tests', function() {
+	it('Unique ID validation', function() {
+		var object = {};
+		sync(object);
+		expect(object.uniqueId).not.toBe(undefined);
+	});
+});
+
+describe('Object initial reset on synconization', function () { 
+	it('Link to an object without arguments', function () { 
+		var object1 = {
+				foo: 'bar'
+			},
+			object2 = {
+				foo: 'baz'
+			};
+		sync(object1)
+		sync(object2).link().to(object1);
+		expect(object2.foo).toBe('bar');
+	});
+
+	it('Link to an object with arguments', function () { 
+		var object1 = {
+				foo: 'bar'
+			},
+			object2 = {
+				foo: 'baz'
+			};
+		sync(object1)
+		sync(object2).link('foo').to(object1);
+		expect(object2.foo).toBe('bar');
+	});
+});
+
+describe('Object syncronization on value change.', function() {
+	it('First value change', function() {
+		var object1 = {
+				foo: 'bar'
+			},
+			object2 = {
+				foo: 'baz'
+			};
+		sync(object1);
+		sync(object2).link().to(object1);
+		object1.foo = 'qux';
+		expect(object2.foo).toBe('qux');
+	});
+});
